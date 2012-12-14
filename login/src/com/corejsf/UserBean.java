d14a7016b8a65e4df808f09d17b8501e07092889
Tanzeel Kazi <tanzeel.kazi@zeuslearning.com>
2012-12-14 16:45:31 +0530

    Commit on 14-Dec-2012 16:45 +0530

--- login/src/com/corejsf/UserBean.java
+++ login/src/com/corejsf/UserBean.java
@@ -7,17 +7,26 @@ import javax.enterprise.context.SessionScoped;
 @Named("user")
 @SessionScoped
 public class UserBean implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private String name;
-	private String password;
-	
-	public String getName() { return name; }
-	public void setName(String newValue) { name = newValue; }
-	
-	public String getPassword() { return password; }
-	public void setPassword(String newValue) { password = newValue; }
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private String password;
+    
+    public String getName() { return name; }
+    public void setName(String newValue) { name = newValue; }
+    
+    public String getPassword() { return password; }
+    public void setPassword(String newValue) { password = newValue; }
+    
+    public String getGreeting() {
+        String greeting = "";
+        if (name != null && !name.isEmpty()) {
+            greeting = "Welcome to JSF2 + Ajax, " + name + "!";
+        }
+        
+        return greeting;
+    }
 }
